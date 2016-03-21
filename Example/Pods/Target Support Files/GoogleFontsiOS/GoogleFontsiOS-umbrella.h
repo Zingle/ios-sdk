d4df6cbe8eb9601f4214340d7dd553876db3ee37
Ryan Farley <rfarley@zingleme.com>
2016-03-21 16:43:41 -0700

    Update pod spec

---
+++ Example/Pods/Target Support Files/GoogleFontsiOS/GoogleFontsiOS-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "GFIFontLoader.h"
+#import "UIFont+OpenSans.h"
+
+FOUNDATION_EXPORT double GoogleFontsiOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char GoogleFontsiOSVersionString[];
+

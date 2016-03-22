2949aa429c659c8cee936d8704ad0812d8b19ba1
Ryan Farley <rfarley@zingleme.com>
2016-03-22 13:37:42 -0700

    Update property name

--- Pod/Classes/Models/ZNGContactFieldValues/ZNGNewContactFieldValue.h
+++ Pod/Classes/Models/ZNGContactFieldValues/ZNGNewContactFieldValue.h
@@ -11,7 +11,7 @@
 @interface ZNGNewContactFieldValue : MTLModel<MTLJSONSerializing>
 
 @property(nonatomic, strong) NSString* value;
-@property(nonatomic, strong) NSString* selectedCustomFieldOptionId;
+@property(nonatomic, strong) NSString* customFieldOptionId;
 @property(nonatomic, strong) NSString* customFieldId;
 
 @end

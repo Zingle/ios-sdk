6ce8d779a769e4b3ff88ae004e26c7e491dd1645
Jason Neel <jason.neel@gmail.com>
2016-06-06 15:18:41 -0700

    Custom field object will now tolerate both string and numeric values in incoming JSON

--- Pod/Classes/Models/ZNGContactFieldValues/ZNGContactFieldValue.m
+++ Pod/Classes/Models/ZNGContactFieldValues/ZNGContactFieldValue.m
@@ -24,4 +24,20 @@
     return [MTLJSONAdapter dictionaryTransformerWithModelClass:ZNGContactField.class];
 }
 
++ (NSValueTransformer *)JSONTransformerForKey:(NSString *)key
+{
+    // Handle the case of value being an int.  We could reasonably ask the server to send strings only, but this is easy enough.
+    if ([key isEqualToString:@"value"]) {
+        return [MTLValueTransformer transformerUsingForwardBlock:^id(id value, BOOL *success, NSError *__autoreleasing *error) {
+            if ([value isKindOfClass:[NSNumber class]]) {
+                return [value stringValue];
+            }
+            
+            return value;
+        }];
+    }
+    
+    return nil;
+}
+
 @end

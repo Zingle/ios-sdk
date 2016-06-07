817aaef88638697436ab80c194d482dfec304310
Jason Neel <jason.neel@gmail.com>
2016-06-07 09:46:47 -0700

    Added types to ZNGService collections

--- Pod/Classes/Models/ZNGService/ZNGService.h
+++ Pod/Classes/Models/ZNGService/ZNGService.h
@@ -12,6 +12,12 @@
 #import "ZNGServiceAddress.h"
 #import "ZNGChannelType.h"
 
+@class ZNGChannel;
+@class ZNGChannelType;
+@class ZNGLabel;
+@class ZNGContactField;
+@class ZNGSetting;
+
 @interface ZNGService : MTLModel<MTLJSONSerializing>
 
 @property(nonatomic, strong) NSString* serviceId;
@@ -20,11 +26,11 @@
 @property(nonatomic, strong) NSString* timeZone;
 @property(nonatomic, strong) ZNGAccount* account;
 @property(nonatomic, strong) ZNGAccountPlan* plan;
-@property(nonatomic, strong) NSArray* channels;
-@property(nonatomic, strong) NSArray* channelTypes;
-@property(nonatomic, strong) NSArray* contactLabels;
-@property(nonatomic, strong) NSArray* contactCustomFields;
-@property(nonatomic, strong) NSArray* settings;
+@property(nonatomic, strong) NSArray<ZNGChannel *> * channels;
+@property(nonatomic, strong) NSArray<ZNGChannelType *> * channelTypes;
+@property(nonatomic, strong) NSArray<ZNGLabel *> * contactLabels;
+@property(nonatomic, strong) NSArray<ZNGContactField *> * contactCustomFields;
+@property(nonatomic, strong) NSArray<ZNGSetting *> * settings;
 @property(nonatomic, strong) ZNGServiceAddress* serviceAddress;
 @property(nonatomic, strong) NSDate* createdAt;
 @property(nonatomic, strong) NSDate* updatedAt;

2a825f6217cae3b8b442060482374ee5b74f35f1
Jason Neel <jason.neel@gmail.com>
2016-06-10 09:21:53 -0700

    Added header that includes all inbox filters

---
+++ Pod/Classes/Models/ZNGContact/ZNGInboxDataFilters.h
@@ -0,0 +1,21 @@
+//
+//  ZNGInboxDataFilters.h
+//  Pods
+//
+//  Created by Jason Neel on 6/10/16.
+//
+//
+
+#ifndef ZNGInboxDataFilters_h
+#define ZNGInboxDataFilters_h
+
+#import "ZNGInboxDataClosed.h"
+#import "ZNGInboxDataConfirmed.h"
+#import "ZNGInboxDataLabel.h"
+#import "ZNGInboxDataOpen.h"
+#import "ZNGInboxDataSearch.h"
+#import "ZNGInboxDataStarred.h"
+#import "ZNGInboxDataUnconfirmed.h"
+
+
+#endif /* ZNGInboxDataFilters_h */

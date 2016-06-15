a1beba4faed0b14d56133104e9519524ccb525cc
Jason Neel <jason.neel@gmail.com>
2016-06-15 11:35:03 -0700

    Finished first attempt at contact session initialization

--- Pod/Classes/ZingleContactSession.h
+++ Pod/Classes/ZingleContactSession.h
@@ -9,13 +9,39 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class ZNGContactService;
+@class ZNGError;
+
+@class ZNGAutomationClient;
+@class ZNGContact;
+@class ZNGContactClient;
+@class ZNGMessageClient;
+@class ZNGTemplateClient;
+
 @interface ZingleContactSession : ZingleSession
 
 @property (nonatomic, readonly, nonnull) NSString * channelTypeID;
 @property (nonatomic, readonly, nonnull) NSString * channelValue;
 
+@property (nonatomic, readonly, nullable) ZNGContactService * contactService;
+@property (nonatomic, readonly, nullable) ZNGContact * contact;
 
-- (instancetype) initWithToken:(NSString *)token key:(NSString *)key channelTypeId:(NSString *)channelTypeId channelValue:(NSString *)channelValue;
+@property (nonatomic, strong, nullable) ZNGAutomationClient * automationClient;
+@property (nonatomic, strong, nullable) ZNGContactClient * contactClient;
+@property (nonatomic, strong, nullable) ZNGMessageClient * messageClient;
+@property (nonatomic, strong, nullable) ZNGTemplateClient * templateClient;
+
+/*
+ *  The initializer for a Zingle session in the Contact domain.  This includes both authentication information for the API user (i.e. the develoepr) and a set of identifying
+ *   information for the contact to be sending messages, etc.
+ *
+ *  @param token Token for Zingle API user
+ *  @param key Security key for Zingle API user
+ *  @param channelTypeId An identifier for the channel type, e.g. the identifier for Big Hotel Messaging System
+ *  @param channelValue The channel value for the current user, e.g. joeSchmoe97 for the user name in Big Hotel Messaging System
+ *  @param completion Optional completion block called to indicate success or failure in authenticating the Zingle API user and the provided contact with channelTypeID and channelValue
+ */
+- (instancetype) initWithToken:(NSString *)token key:(NSString *)key channelTypeId:(NSString *)channelTypeId channelValue:(NSString *)channelValue completion:(nullable void (^)(BOOL success, ZNGError * _Nullable error))completion;
 
 @end
 
--- Pod/Classes/ZingleContactSession.m
+++ Pod/Classes/ZingleContactSession.m
@@ -7,19 +7,72 @@
 //
 
 #import "ZingleContactSession.h"
+#import "ZNGLogging.h"
+#import "ZNGContactClient.h"
+#import "ZNGContactServiceClient.h"
+
+static const int zngLogLevel = ZNGLogLevelInfo;
 
 @implementation ZingleContactSession
+{
+    void (^completion)(BOOL success, ZNGError * _Nullable error);
+}
 
-- (instancetype) initWithToken:(NSString *)token key:(NSString *)key channelTypeId:(NSString *)channelTypeId channelValue:(NSString *)channelValue
+- (instancetype) initWithToken:(NSString *)token key:(NSString *)key channelTypeId:(NSString *)channelTypeId channelValue:(NSString *)channelValue completion:(nullable void (^)(BOOL success, ZNGError * _Nullable error))theCompletion
 {
     self = [super initWithToken:token key:key];
     
     if (self != nil) {
         _channelTypeID = [channelTypeId copy];
         _channelValue = [channelValue copy];
+        completion = [theCompletion copy];
+        
+        // First we must find our contact service
+        NSDictionary *parameters = @{ @"channel_value" : channelValue,
+                                      @"channel_type_id" : channelTypeId };
+        [self.contactServiceClient contactServiceListWithParameters:parameters success:^(NSArray *contactServices, ZNGStatus *status) {
+            if ([contactServices count] == 0) {
+                ZNGLogInfo(@"Unable to find a contact service match for value \"%@\" of type \"%@\"", channelValue, channelTypeId);
+                completion(NO, nil);
+                completion = nil;
+                return;
+            }
+            
+            if ([contactServices count] > 1) {
+                ZNGLogWarn(@"Found %lu matches for contact services with value \"%@\" of type \"%@\".  Using first match.",(unsigned long)[contactServices count], channelValue, channelTypeId);
+            }
+            
+            _contactService = [contactServices firstObject];
+            [self findOrCreateContactForContactService];
+        } failure:^(ZNGError *error) {
+            ZNGLogInfo(@"Unable to find a contact service match for value \"%@\" of type \"%@\"", channelValue, channelTypeId);
+            completion(NO, error);
+            completion = nil;
+        }];
     }
     
     return self;
+}
+
+- (void) findOrCreateContactForContactService
+{
+    [self.contactClient findOrCreateContactWithChannelTypeID:_channelTypeID andChannelValue:_channelValue success:^(ZNGContact *contact, ZNGStatus *status) {
+        if (contact == nil) {
+            ZNGLogError(@"Unable to find nor create contact for value \"%@\" of channel type ID \"%@\".  Request returned no result.", _channelValue, _channelTypeID);
+            completion(NO, nil);
+            completion = nil;
+            return;
+        }
+        
+        // We did it!
+        _contact = contact;
+        completion(YES, nil);
+        completion = nil;
+    } failure:^(ZNGError *error) {
+        ZNGLogError(@"Unable to find nor create contact for value \"%@\" of channel type ID \"%@\".  Request failed.", _channelValue, _channelTypeID);
+        completion(NO, error);
+        completion = nil;
+    }];
 }
 
 @end

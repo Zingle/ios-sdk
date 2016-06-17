a94f8560c0f8253aa005aaac86eea4dd6fbbde5d
Jason Neel <jason.neel@gmail.com>
2016-06-17 15:22:30 -0700

    Instance methods != class methods

--- Pod/Classes/ZingleSDK.m
+++ Pod/Classes/ZingleSDK.m
@@ -15,22 +15,22 @@ static const int zngLogLevel = ZNGLogLevelInfo;
 
 @implementation ZingleSDK
 
-- (ZingleContactSession *) contactSessionWithToken:(NSString *)token key:(NSString *)key channelTypeId:(NSString *)channelTypeId channelValue:(NSString *)channelValue
++ (ZingleContactSession *) contactSessionWithToken:(NSString *)token key:(NSString *)key channelTypeId:(NSString *)channelTypeId channelValue:(NSString *)channelValue
 {
     return [self contactSessionWithToken:token key:key channelTypeId:channelTypeId channelValue:channelValue contactServiceChooser:nil];
 }
 
-- (ZingleContactSession *) contactSessionWithToken:(NSString *)token key:(NSString *)key channelTypeId:(NSString *)channelTypeId channelValue:(NSString *)channelValue contactServiceChooser:(nullable ZNGContactServiceChooser)contactServiceChooser
++ (ZingleContactSession *) contactSessionWithToken:(NSString *)token key:(NSString *)key channelTypeId:(NSString *)channelTypeId channelValue:(NSString *)channelValue contactServiceChooser:(nullable ZNGContactServiceChooser)contactServiceChooser
 {
     return [[ZingleContactSession alloc] initWithToken:token key:key channelTypeId:channelTypeId channelValue:channelValue contactServiceChooser:contactServiceChooser];
 }
 
-- (ZingleAccountSession *) accountSessionWithToken:(NSString *)token key:(NSString *)key
++ (ZingleAccountSession *) accountSessionWithToken:(NSString *)token key:(NSString *)key
 {
     return [self accountSessionWithToken:token key:key accountChooser:nil serviceChooser:nil];
 }
 
-- (ZingleAccountSession *) accountSessionWithToken:(NSString *)token key:(NSString *)key accountChooser:(nullable ZNGAccountChooser)accountChooser serviceChooser:(nullable ZNGServiceChooser)serviceChooser
++ (ZingleAccountSession *) accountSessionWithToken:(NSString *)token key:(NSString *)key accountChooser:(nullable ZNGAccountChooser)accountChooser serviceChooser:(nullable ZNGServiceChooser)serviceChooser
 {
     return [[ZingleAccountSession alloc] initWithToken:token key:token accountChooser:accountChooser serviceChooser:serviceChooser];
 }

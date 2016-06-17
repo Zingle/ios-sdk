c016a381c1d0c20fe10505ef88674566ad813299
Jason Neel <jason.neel@gmail.com>
2016-06-17 15:49:30 -0700

    Added NS_UNAVAILABLE annotations to disallow certain initialization methods on session objects

--- Pod/Classes/ZingleSpecificAccountSession.h
+++ Pod/Classes/ZingleSpecificAccountSession.h
@@ -7,11 +7,14 @@
 //
 
 #import "ZingleSession.h"
+#import "ZingleSDK.h"
 
 @class ZNGAccount;
 @class ZingleAccountSession;
 @class ZNGService;
 @class ZNGAutomationClient, ZNGContactChannelClient, ZNGContactClient, ZNGLabelClient, ZNGMessageClient, ZNGTemplateClient;
+
+NS_ASSUME_NONNULL_BEGIN
 
 /**
  *  Private subclass of ZingleAccountSession that will always have session and account information available.
@@ -30,5 +33,11 @@
 @property (nonatomic, strong, nonnull) ZNGTemplateClient * templateClient;
 
 - (nonnull instancetype) initWithAccountSession:(nonnull __weak ZingleAccountSession *)accountSession account:(nonnull ZNGAccount *)account service:(nonnull ZNGService *)service;
+
+// Disallowed initializers for this private subclass:
+- (nonnull instancetype) initWithToken:(nonnull NSString *)token key:(nonnull NSString *)key NS_UNAVAILABLE;
+- (instancetype) initWithToken:(NSString *)token key:(NSString *)key accountChooser:(nullable ZNGAccountChooser)accountChooser serviceChooser:(nullable ZNGServiceChooser)serviceChooser NS_UNAVAILABLE;
+
+NS_ASSUME_NONNULL_END
 
 @end

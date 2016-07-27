7e445fe5ac3bfc395dd1db85a29b3df4367b35f7
Jason Neel <jason.neel@gmail.com>
2016-07-27 11:49:20 -0700

    MOBILE-113: Error dialog should probably not permanentally capture the UI

--- Pod/Classes/UI/Controllers/ZNGServiceToContactViewController.m
+++ Pod/Classes/UI/Controllers/ZNGServiceToContactViewController.m
@@ -426,6 +426,7 @@ static void * KVOContext = &KVOContext;
     [self.conversation addInternalNote:note success:nil failure:^(ZNGError * _Nonnull error) {
         UIAlertController * alert = [UIAlertController alertControllerWithTitle:@"Failed to add note" message:nil preferredStyle:UIAlertControllerStyleAlert];
         UIAlertAction * ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+        [alert addAction:ok];
         [self presentViewController:alert animated:YES completion:nil];
     }];
 }

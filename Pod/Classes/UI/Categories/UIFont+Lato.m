f174ee29ff8693b4b142d56fdfc47102ebc20fc6
Jason Neel <jason.neel@gmail.com>
2016-08-04 18:11:54 -0700

    MOBILE-172: Fonts are now loaded from the SDK bundle on launch

--- Pod/Classes/UI/Categories/UIFont+Lato.m
+++ Pod/Classes/UI/Categories/UIFont+Lato.m
@@ -7,8 +7,54 @@
 //
 
 #import "UIFont+Lato.h"
+#import "ZingleSDK.h"
+#import "ZNGLogging.h"
+@import CoreText;
+
+static const int zngLogLevel = ZNGLogLevelWarning;
 
 @implementation UIFont (Lato)
+
++ (void) load
+{
+    NSArray<NSData *> * fontDataArray = @[
+                                          [self fontDataForFont:@"Lato-Regular"],
+                                          [self fontDataForFont:@"Lato-Semibold"],
+                                          [self fontDataForFont:@"Lato-Bold"]
+                                          ];
+
+    for (NSData * fontData in fontDataArray) {
+        CGDataProviderRef provider = CGDataProviderCreateWithCFData((CFDataRef)fontData);
+        CGFontRef font = CGFontCreateWithDataProvider(provider);
+        CFErrorRef error;
+        
+        if (!CTFontManagerRegisterGraphicsFont(font, &error)) {
+            CFStringRef errorDescription = CFErrorCopyDescription(error);
+            ZNGLogError(@"Unable to load Lato font data");
+            CFRelease(errorDescription);
+        }
+        
+        CFRelease(font);
+        CFRelease(provider);
+    }
+}
+
++ (NSData *) fontDataForFont:(NSString *)fontName
+{
+    NSBundle * bundle = [NSBundle bundleForClass:[ZingleSDK class]];
+    NSString * path = [bundle pathForResource:fontName ofType:@"ttf"];
+    NSData * fontData = nil;
+    
+    if (path != nil) {
+        fontData = [NSData dataWithContentsOfFile:path];
+    }
+    
+    if (fontData != nil) {
+        return fontData;
+    }
+    
+    return nil;
+}
 
 + (UIFont *) latoFontOfSize:(CGFloat)size
 {

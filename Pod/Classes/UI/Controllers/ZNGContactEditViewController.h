6eb361e821db9b1960826381b7118061bb3733ab
Jason Neel <jason.neel@gmail.com>
2016-08-23 13:51:50 -0700

    MOBILE-112: Added contact editing view controller to project

---
+++ Pod/Classes/UI/Controllers/ZNGContactEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZNGContactEditViewController.h
+//  Pods
+//
+//  Created by Jason Neel on 8/23/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZNGContactEditViewController : UIViewController
+
+@end

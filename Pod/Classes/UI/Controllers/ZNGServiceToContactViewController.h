d9cc01258fc647c35cb7b98fe9507588a3629963
Jason Neel <jason.neel@gmail.com>
2016-08-22 18:22:18 -0700

    MOBILE-112: Added conversation view controller delegate that can prevent the conversation view from displaying its own edit contact view controller

--- Pod/Classes/UI/Controllers/ZNGServiceToContactViewController.h
+++ Pod/Classes/UI/Controllers/ZNGServiceToContactViewController.h
@@ -9,8 +9,24 @@
 #import <ZingleSDK/ZNGConversationViewController.h>
 #import "ZNGConversationServiceToContact.h"
 
+@protocol  ZNGServiceToContactViewDelegate <NSObject>
+
+@optional
+
+/**
+ *  Optional delegate method that is called when the user pressed 'Edit Contact.'
+ *  If this method is implemented and returns NO, the edit contact screen will not be shown, instead allowing the delegate
+ *   to present UI.
+ */
+- (BOOL) shouldShowEditContactScreenForContact:(nonnull ZNGContact *)contact;
+
+@end
+
+
 @interface ZNGServiceToContactViewController : ZNGConversationViewController
 
 @property (nonatomic, strong, nullable) ZNGConversationServiceToContact * conversation;
+
+@property (nonatomic, weak, nullable) id <ZNGServiceToContactViewDelegate> delegate;
 
 @end

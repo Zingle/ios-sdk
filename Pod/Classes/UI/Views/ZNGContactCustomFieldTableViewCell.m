be583d9c525c33a2a54f76fe90aa8b176cb5ae25
Jason Neel <jason.neel@gmail.com>
2016-08-26 11:05:00 -0700

    MOBILE-112: Initial work on using UIPicker for single select custom fields

--- Pod/Classes/UI/Views/ZNGContactCustomFieldTableViewCell.m
+++ Pod/Classes/UI/Views/ZNGContactCustomFieldTableViewCell.m
@@ -39,9 +39,16 @@ static const int zngLogLevel = ZNGLogLevelInfo;
     } else if ([self.customFieldValue.customField.dataType isEqualToString:ZNGContactFieldDataTypeDate]) {
         datePicker = [[UIDatePicker alloc] init];
         datePicker.datePickerMode = UIDatePickerModeDate;
+    } else if (([self.customFieldValue.customField.dataType isEqualToString:ZNGContactFieldDataTypeSingleSelect]) || ([self.customFieldValue.customField.dataType isEqualToString:ZNGContactFieldDataTypeBool])) {
+        pickerView = [[UIPickerView alloc] init];
+        pickerView.delegate = self;
+        self.textField.inputView = pickerView;
     }
     
-    [datePicker addTarget:self action:@selector(datePickerSelectionChanged:) forControlEvents:UIControlEventValueChanged];
+    if (datePicker != nil) {
+        [datePicker addTarget:self action:@selector(datePickerSelectionChanged:) forControlEvents:UIControlEventValueChanged];
+        self.textField.inputView = datePicker;
+    }
 }
 
 - (void) datePickerSelectionChanged:(id)sender
@@ -49,22 +56,70 @@ static const int zngLogLevel = ZNGLogLevelInfo;
     
 }
 
-- (BOOL) textFieldShouldBeginEditing:(UITextField *)textField
+#pragma mark - Picker view
+- (void) pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
 {
-    if ([self.customFieldValue.customField.dataType isEqualToString:ZNGContactFieldDataTypeString]) {
-        // We want a string.  We don't have to do anything special.
-        textField.keyboardType = UIKeyboardTypeDefault;
-        return YES;
+    NSString * value;
+    
+    if ([self.customFieldValue.customField.dataType isEqualToString:ZNGContactFieldDataTypeBool]) {
+        value = (row == 0) ? @"No" : @"Yes";
+    } else {
+        value = self.customFieldValue.customField.options[row];
     }
     
-    if ([self.customFieldValue.customField.dataType isEqualToString:ZNGContactFieldDataTypeNumber]) {
-        // Numbers only.
-        textField.keyboardType = UIKeyboardTypeNumberPad;
-        return YES;
-    }
-    
-    
+    self.textField.text = value;
 }
+
+- (NSInteger) numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+- (NSInteger) pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    if ([self.customFieldValue.customField.dataType isEqualToString:ZNGContactFieldDataTypeBool]) {
+        return 2;
+    }
+    
+    return [self.customFieldValue.customField.options count];
+}
+
+- (NSString *) pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    if ([self.customFieldValue.customField.dataType isEqualToString:ZNGContactFieldDataTypeBool]) {
+        if (row == 0) {
+            return @"No";
+        } else {
+            return @"Yes";
+        }
+    }
+    
+    if (row >= [self.customFieldValue.customField.options count]) {
+        ZNGLogError(@"Out of bounds when retrieving single select custom field picker options.  %lld >= our total of %llu options", (long long)row, (unsigned long long)[self.customFieldValue.customField.options count]);
+        return nil;
+    }
+    
+    return self.customFieldValue.customField.options[row];
+}
+
+#pragma mark - Text field delegate
+//
+//- (BOOL) textFieldShouldBeginEditing:(UITextField *)textField
+//{
+//    if ([self.customFieldValue.customField.dataType isEqualToString:ZNGContactFieldDataTypeString]) {
+//        // We want a string.  We don't have to do anything special.
+//        textField.keyboardType = UIKeyboardTypeDefault;
+//        return YES;
+//    }
+//    
+//    if ([self.customFieldValue.customField.dataType isEqualToString:ZNGContactFieldDataTypeNumber]) {
+//        // Numbers only.
+//        textField.keyboardType = UIKeyboardTypeNumberPad;
+//        return YES;
+//    }
+//    
+//    
+//}
 
 - (void) textFieldDidEndEditing:(UITextField *)textField
 {

f9b5332f2b0c199a45b9171f0c4f54e19f9063ae
Jason Neel <jason.neel@gmail.com>
2016-09-20 09:54:20 -0700

    Updated example project pods

--- Example/Pods/Target Support Files/GoogleFontsiOS-iOS8.0/GoogleFontsiOS-iOS8.0-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GoogleFontsiOS_iOS8_0 : NSObject
-@end
-@implementation PodsDummy_GoogleFontsiOS_iOS8_0
-@end
--- Example/Pods/Target Support Files/GoogleFontsiOS-iOS9.3/GoogleFontsiOS-iOS9.3-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "GFIFontLoader.h"
-#import "UIFont+OpenSans.h"
-
-FOUNDATION_EXPORT double GoogleFontsiOSVersionNumber;
-FOUNDATION_EXPORT const unsigned char GoogleFontsiOSVersionString[];
-

85b2b26b6eb42b5d496451694c24eba5dadfc2db
Jason Neel <jason.neel@gmail.com>
2016-10-24 16:02:05 -0700

    MOBILE-259: Added share button to image view controller

--- Pod/Classes/UI/Controllers/ZNGImageViewController.m
+++ Pod/Classes/UI/Controllers/ZNGImageViewController.m
@@ -21,13 +21,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     self.imageView.image = self.image;
+    
+    UIBarButtonItem * shareButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(pressedShare:)];
+    self.navigationItem.rightBarButtonItem = shareButton;
 }
 
 - (void) setImage:(UIImage *)image
 {
     _image = image;
     self.imageView.image = image;
+}
+                                     
+- (void) pressedShare:(id)sender
+{
+    UIActivityViewController * shareView = [[UIActivityViewController alloc] initWithActivityItems:@[self.image] applicationActivities:nil];
+    shareView.popoverPresentationController.barButtonItem = self.navigationItem.rightBarButtonItem;
+    [self presentViewController:shareView animated:YES completion:nil];
 }
 
 @end

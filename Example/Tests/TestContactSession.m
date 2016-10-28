36d198d71f7f4a9c5804aa67157e35f222fc8a37
Jason Neel <jason.neel@gmail.com>
2016-10-27 17:57:32 -0700

    Contact session tests!  More of them!

--- Example/Tests/TestContactSession.m
+++ Example/Tests/TestContactSession.m
@@ -151,4 +151,84 @@
     }];
 }
 
+- (void) testMultipleContactSessions
+{
+    ZingleContactSession * session = [[ZingleContactSession alloc] initWithToken:@"token" key:@"key" channelTypeId:@"anID" channelValue:@"aValue" contactServiceChooser:nil errorHandler:nil];
+    
+    // Set the HTTP client to nil to prevent reaching out through the internet tubes if we forget to stub something
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wnonnull"
+    session.sessionManager = nil;
+#pragma clang diagnostic pop
+    
+    ZNGMockUserAuthorizationClient * authClient = [[ZNGMockUserAuthorizationClient alloc] initWithSession:session];
+    authClient.contact = me;
+    session.userAuthorizationClient = authClient;
+    
+    ZNGMockAccountClient * accountClient = [[ZNGMockAccountClient alloc] initWithSession:session];
+    accountClient.accounts = @[account1];
+    session.accountClient = accountClient;
+    
+    ZNGMockContactServiceClient * contactServiceClient = [[ZNGMockContactServiceClient alloc] initWithSession:session];
+    NSArray<ZNGContactService *> * contactServices = @[contactService1, contactService2];
+    contactServiceClient.contactServices = contactServices;
+    session.contactServiceClient = contactServiceClient;
+    
+    [self keyValueObservingExpectationForObject:session keyPath:NSStringFromSelector(@selector(availableContactServices)) handler:^BOOL(id  _Nonnull observedObject, NSDictionary * _Nonnull change) {
+        return [session.availableContactServices isEqualToArray:contactServices];
+    }];
+    
+    [session connect];
+    
+    [self waitForExpectationsWithTimeout:2.0 handler:^(NSError * _Nullable error) {
+        
+        [self keyValueObservingExpectationForObject:session keyPath:NSStringFromSelector(@selector(available)) expectedValue:@(YES)];
+        
+        ZNGMockContactClient * contactClient = [[ZNGMockContactClient alloc] initWithSession:session serviceId:contactService1.serviceId];
+        contactClient.contact = me;
+        
+        session.contactClient = contactClient;
+        session.contactService = contactService1;
+        
+        [self waitForExpectationsWithTimeout:2.0 handler:nil];
+    }];
+}
+
+- (void) testContactServiceChoosingBlock
+{
+    XCTestExpectation * contactServiceChooserExpectation = [self expectationWithDescription:@"Contact chooser block is called"];
+    ZingleContactSession * session = [[ZingleContactSession alloc] initWithToken:@"token" key:@"key" channelTypeId:@"anID" channelValue:@"aValue" contactServiceChooser:^ZNGContactService * _Nullable(NSArray<ZNGContactService *> * _Nonnull availableContactServices) {
+        [contactServiceChooserExpectation fulfill];
+        return contactService1;
+    } errorHandler:nil];
+    
+    // Set the HTTP client to nil to prevent reaching out through the internet tubes if we forget to stub something
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wnonnull"
+    session.sessionManager = nil;
+#pragma clang diagnostic pop
+    
+    ZNGMockUserAuthorizationClient * authClient = [[ZNGMockUserAuthorizationClient alloc] initWithSession:session];
+    authClient.contact = me;
+    session.userAuthorizationClient = authClient;
+    
+    ZNGMockAccountClient * accountClient = [[ZNGMockAccountClient alloc] initWithSession:session];
+    accountClient.accounts = @[account1];
+    session.accountClient = accountClient;
+    
+    ZNGMockContactServiceClient * contactServiceClient = [[ZNGMockContactServiceClient alloc] initWithSession:session];
+    NSArray<ZNGContactService *> * contactServices = @[contactService1, contactService2];
+    contactServiceClient.contactServices = contactServices;
+    session.contactServiceClient = contactServiceClient;
+    
+    [self keyValueObservingExpectationForObject:session keyPath:NSStringFromSelector(@selector(availableContactServices)) handler:^BOOL(id  _Nonnull observedObject, NSDictionary * _Nonnull change) {
+        return ([session.availableContactServices count] > 0);
+    }];
+    [self keyValueObservingExpectationForObject:session keyPath:NSStringFromSelector(@selector(contactService)) expectedValue:contactService1];
+    
+    [session connect];
+    
+    [self waitForExpectationsWithTimeout:2.0 handler:nil];
+}
+
 @end

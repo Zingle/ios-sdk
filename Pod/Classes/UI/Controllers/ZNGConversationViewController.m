8e657c585768ddccf927c636a9ce14238574d252
Jason Neel <jason.neel@gmail.com>
2016-11-15 16:25:50 -0800

    MOBILE-417: Added transparency to sending messages

--- Pod/Classes/UI/Controllers/ZNGConversationViewController.m
+++ Pod/Classes/UI/Controllers/ZNGConversationViewController.m
@@ -943,6 +943,8 @@ static void * ZNGConversationKVOContext  =   &ZNGConversationKVOContext;
         JSQMessagesCollectionViewCell * cell = (JSQMessagesCollectionViewCell *)[super collectionView:collectionView cellForItemAtIndexPath:indexPath];
         cell.cellTopLabel.numberOfLines = 0;    // Support multiple lines
         
+        cell.alpha = event.message.sending ? 0.5 : 1.0;
+        
         UIColor * textColor;
         
         if ([event isNote]) {

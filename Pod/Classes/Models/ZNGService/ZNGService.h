7abfc85c6ee8966da20c3d0d3db27f26f74a4abc
Jason Neel <jason.neel@gmail.com>
2016-11-28 13:32:14 -0800

    Added nullability notations to ZNGService

--- Pod/Classes/Models/ZNGService/ZNGService.h
+++ Pod/Classes/Models/ZNGService/ZNGService.h
@@ -22,38 +22,38 @@
 
 @interface ZNGService : MTLModel<MTLJSONSerializing>
 
-@property(nonatomic, strong) NSString* serviceId;
-@property(nonatomic, strong) NSString* displayName;
-@property(nonatomic, strong) NSString* businessName;
-@property(nonatomic, strong) NSString* timeZone;
-@property(nonatomic, strong) ZNGAccount* account;
-@property(nonatomic, strong) ZNGAccountPlan* plan;
-@property(nonatomic, strong) NSArray<ZNGChannel *> * channels;
-@property(nonatomic, strong) NSArray<ZNGChannelType *> * channelTypes;
-@property(nonatomic, strong) NSArray<ZNGLabel *> * contactLabels;
-@property(nonatomic, strong) NSArray<ZNGContactField *> * contactCustomFields;
-@property(nonatomic, strong) NSArray<ZNGSetting *> * settings;
-@property(nonatomic, strong) NSArray<ZNGAutomation *> * automations;
-@property(nonatomic, strong) NSArray<ZNGTemplate *> * templates;
-@property(nonatomic, strong) ZNGServiceAddress* serviceAddress;
-@property(nonatomic, strong) NSDate* createdAt;
-@property(nonatomic, strong) NSDate* updatedAt;
+@property(nonatomic, strong, nonnull) NSString* serviceId;
+@property(nonatomic, strong, nullable) NSString* displayName;
+@property(nonatomic, strong, nullable) NSString* businessName;
+@property(nonatomic, strong, nullable) NSString* timeZone;
+@property(nonatomic, strong, nullable) ZNGAccount* account;
+@property(nonatomic, strong, nullable) ZNGAccountPlan* plan;
+@property(nonatomic, strong, nullable) NSArray<ZNGChannel *> * channels;
+@property(nonatomic, strong, nullable) NSArray<ZNGChannelType *> * channelTypes;
+@property(nonatomic, strong, nullable) NSArray<ZNGLabel *> * contactLabels;
+@property(nonatomic, strong, nullable) NSArray<ZNGContactField *> * contactCustomFields;
+@property(nonatomic, strong, nullable) NSArray<ZNGSetting *> * settings;
+@property(nonatomic, strong, nullable) NSArray<ZNGAutomation *> * automations;
+@property(nonatomic, strong, nullable) NSArray<ZNGTemplate *> * templates;
+@property(nonatomic, strong, nullable) ZNGServiceAddress* serviceAddress;
+@property(nonatomic, strong, nullable) NSDate* createdAt;
+@property(nonatomic, strong, nullable) NSDate* updatedAt;
 
-- (NSArray<ZNGAutomation *> *) activeAutomations;
+- (NSArray<ZNGAutomation *> * _Nullable) activeAutomations;
 
 - (BOOL) isTextRelay;
 
-- (ZNGChannelType *)phoneNumberChannelType;
-- (ZNGChannelType *)channelTypeWithDisplayName:(NSString *)channelDisplayName;
-- (ZNGChannelType *)channelTypeWithTypeClass:(NSString *)typeClass;
+- (ZNGChannelType * _Nullable)phoneNumberChannelType;
+- (ZNGChannelType * _Nullable)channelTypeWithDisplayName:(NSString * _Nonnull)channelDisplayName;
+- (ZNGChannelType * _Nullable)channelTypeWithTypeClass:(NSString * _Nonnull)typeClass;
 
-- (ZNGChannel *)defaultPhoneNumberChannel;
-- (ZNGChannel *)defaultChannelForType:(ZNGChannelType *)channelType;
+- (ZNGChannel * _Nullable)defaultPhoneNumberChannel;
+- (ZNGChannel * _Nullable)defaultChannelForType:(ZNGChannelType * _Nonnull)channelType;
 
 /**
  *  Thanks to http://jira.zinglecorp.com:8080/browse/TECH-1970, there is logic for displaying a channel
  *   that depends on the country code for the channel vs. the service.  This is gross and I hate it.
  */
-- (BOOL) shouldDisplayRawValueForChannel:(ZNGChannel *)channel;
+- (BOOL) shouldDisplayRawValueForChannel:(ZNGChannel * _Nonnull)channel;
 
 @end

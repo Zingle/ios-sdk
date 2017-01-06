dee6d6d969c3a5c594c27b173eef8b0cab2c7038
Jason Neel <jason.neel@gmail.com>
2017-01-06 10:34:40 -0800

    Image attachments now keep a cached, resized version of their image for faster drawing

--- Pod/Classes/UI/Layout/ZNGImageAttachment.m
+++ Pod/Classes/UI/Layout/ZNGImageAttachment.m
@@ -9,6 +9,10 @@
 #import "ZNGImageAttachment.h"
 
 @implementation ZNGImageAttachment
+{
+    CGFloat imageScale;
+    UIImage * scaledImage;
+}
 
 - (id) initWithData:(NSData *)contentData ofType:(NSString *)uti
 {
@@ -19,6 +23,41 @@
     }
     
     return self;
+}
+
+- (UIImage *) imageForBounds:(CGRect)imageBounds textContainer:(NSTextContainer *)textContainer characterIndex:(NSUInteger)charIndex
+{
+    return scaledImage ?: [super imageForBounds:imageBounds textContainer:textContainer characterIndex:charIndex];
+}
+
+- (void) setImageScale:(CGFloat)newImageScale
+{
+    if (imageScale == newImageScale) {
+        return;
+    }
+    
+    imageScale = newImageScale;
+    scaledImage = nil;
+    
+    if ((newImageScale == 0.0) || (newImageScale == 1.0)) {
+        // We can use default scale.
+        return;
+    }
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+        CGFloat width = self.image.size.width * newImageScale;
+        CGFloat height = self.image.size.height * newImageScale;
+        CGSize newSize = CGSizeMake(width, height);
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
+        [self.image drawInRect:CGRectMake(0.0, 0.0, width, height)];
+        UIImage * newScaledImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            scaledImage = newScaledImage;
+        });
+    });
 }
 
 - (CGRect) attachmentBoundsForTextContainer:(NSTextContainer *)textContainer proposedLineFragment:(CGRect)lineFrag glyphPosition:(CGPoint)position characterIndex:(NSUInteger)charIndex
@@ -32,10 +71,14 @@
     
     if ((downscaleHeight >= 1.0) && (downscaleWidth >= 1.0)) {
         // Our image will fit as is
+        [self setImageScale:1.0];
         return [super attachmentBoundsForTextContainer:textContainer proposedLineFragment:lineFrag glyphPosition:position characterIndex:charIndex];
     }
     
     CGFloat downscale = MIN(downscaleWidth, downscaleHeight);
+    
+    [self setImageScale:downscale];
+    
     return CGRectIntegral(CGRectMake(0.0, 0.0, self.image.size.width * downscale, self.image.size.height * downscale));
 }
 

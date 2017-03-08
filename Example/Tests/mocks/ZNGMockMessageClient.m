9fae026e2898acee8b6532193ea268e4f16d93b6
Jason Neel <jason.neel@gmail.com>
2017-03-08 11:10:08 -0800

    IOS-597,IOS-599: Mock message client should not set the last message property until after its fake network traffic delay

--- Example/Tests/mocks/ZNGMockMessageClient.m
+++ Example/Tests/mocks/ZNGMockMessageClient.m
@@ -16,12 +16,11 @@
             success:(void (^)(ZNGNewMessageResponse * message, ZNGStatus* status))success
             failure:(void (^)(ZNGError* error))failure
 {
-    self.lastSentMessageAttachments = newMessage.attachments;
-    
     ZNGStatus * status = [[ZNGStatus alloc] init];
     status.statusCode = 200;
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.lastSentMessageAttachments = newMessage.attachments;
         success(nil, status);
     });
 }

7a9d1b983d07628dfee357472af8362411a26aaf
Jason Neel <jason.neel@gmail.com>
2017-04-06 14:49:59 -0700

    Modified mocked message client to allow failures and to send back message IDs on success

---
+++ Example/Tests/TestMessageSending.m
@@ -0,0 +1,39 @@
+//
+//  TestMessageSending.m
+//  ZingleSDK
+//
+//  Created by Jason Neel on 4/6/17.
+//  Copyright © 2017 Zingle. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestMessageSending : XCTestCase
+
+@end
+
+@implementation TestMessageSending
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

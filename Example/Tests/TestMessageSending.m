d9579b8e2eda0564e96383c3341c07e26cce28d0
Jason Neel <jason.neel@gmail.com>
2017-04-06 14:51:25 -0700

    Added message sending completion and failure block tests.  Verified that they fail with code older than this branch.

--- Example/Tests/TestMessageSending.m
+++ Example/Tests/TestMessageSending.m
@@ -7,33 +7,73 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <ZingleSDK/ZingleAccountSession.h>
+#import <ZingleSDK/ZNGConversationServiceToContact.h>
+#import <ZingleSDK/ZNGSocketClient.h>
+#import "ZNGMockMessageClient.h"
+#import "ZNGMockContactClient.h"
+#import "ZNGMockServiceClient.h"
+#import "ZNGStubbedEventClient.h"
 
 @interface TestMessageSending : XCTestCase
 
 @end
 
 @implementation TestMessageSending
+{
+    ZNGMockMessageClient * messageClient;
+    ZNGConversationServiceToContact * conversation;
+}
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    ZingleAccountSession * session = [ZingleSDK accountSessionWithToken:@"token" key:@"key"];
+    ZNGSocketClient * socketClient = [[ZNGSocketClient alloc] initWithSession:session];
+    
+    ZNGChannel * channel = [[ZNGChannel alloc] init];
+    ZNGChannelType * channelType = [[ZNGChannelType alloc] init];
+    channelType.channelTypeId = @"1111-22222222222-333333333333-4444";
+    channel.channelType = channelType;
+    
+    messageClient = [[ZNGMockMessageClient alloc] init];
+    
+    ZNGService * service = [[ZNGService alloc] init];
+    ZNGContact * contact = [[ZNGContact alloc] init];
+    contact.contactId = @"1234-ABCDEFABCDEFABCD-EFABCDEFABCD-1234";
+    ZNGStubbedEventClient * eventClient = [[ZNGStubbedEventClient alloc] init];
+    ZNGMockContactClient * contactClient = [[ZNGMockContactClient alloc] init];
+    
+    conversation = [[ZNGConversationServiceToContact alloc] initFromService:service toContact:contact withCurrentUserId:@"" usingChannel:channel withMessageClient:messageClient eventClient:eventClient contactClient:contactClient socketClient:socketClient];
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+- (void) testCompletionBlockCalled
+{
+    XCTestExpectation * expectation = [self expectationWithDescription:@"Completion block called after sending message"];
+    
+    [conversation sendMessageWithBody:@"body" imageData:nil uuid:nil success:^(ZNGStatus * _Nullable status) {
+        [expectation fulfill];
+    } failure:^(ZNGError * _Nullable error) {
+        XCTFail(@"Failure block was called when test should trigger success");
     }];
+    
+    [self waitForExpectationsWithTimeout:3.0 handler:nil];
+}
+
+- (void) testFailureBlockCalled
+{
+    XCTestExpectation * expectation = [self expectationWithDescription:@"Completion block called after sending message"];
+    
+    messageClient.alwaysFail = YES;
+    
+    [conversation sendMessageWithBody:@"body" imageData:nil uuid:nil success:^(ZNGStatus * _Nullable status) {
+        XCTFail(@"Success block was called when test should trigger failure");
+    } failure:^(ZNGError * _Nullable error) {
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:3.0 handler:nil];
+    messageClient.alwaysFail = NO;
 }
 
 @end

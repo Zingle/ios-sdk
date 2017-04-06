95cb9ac77124b5a73d59d99405aba93d8fd14982
Jason Neel <jason.neel@gmail.com>
2017-04-06 14:50:20 -0700

    Modified stubbed event client to always call success blocks

--- Example/Tests/mocks/ZNGStubbedEventClient.m
+++ Example/Tests/mocks/ZNGStubbedEventClient.m
@@ -14,14 +14,22 @@
                         success:(void (^)(NSArray<ZNGEvent *> * events, ZNGStatus* status))success
                         failure:(void (^)(ZNGError* error))failure
 {
-    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if (success) {
+            success(nil, nil);
+        }
+    });
 }
 
 - (void)eventWithId:(NSString*)eventId
             success:(void (^)(ZNGEvent * event, ZNGStatus* status))success
             failure:(void (^)(ZNGError* error))failure
 {
-    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if (success) {
+            success(nil, nil);
+        }
+    });
 }
 
 - (void)postInternalNote:(NSString *)note
@@ -29,7 +37,11 @@
                  success:(void (^)(ZNGEvent * note, ZNGStatus * status))success
                  failure:(void (^)(ZNGError * error))failure
 {
-    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if (success) {
+            success(nil, nil);
+        }
+    });
 }
 
 @end

1a6c827fe913de0d3c0bca1d00cba2115d3dd746
Jason Neel <jason.neel@gmail.com>
2017-04-12 15:16:59 -0700

    IOS-610: Added avatar image view subclass that places the edit icon vertically centered

---
+++ Pod/Classes/UI/Views/ZNGAvatarImageViewVerticallyCentered.h
@@ -0,0 +1,13 @@
+//
+//  ZNGAvatarImageViewVerticallyCentered.h
+//  Pods
+//
+//  Created by Jason Neel on 4/12/17.
+//
+//
+
+#import "ZNGAvatarImageView.h"
+
+@interface ZNGAvatarImageViewVerticallyCentered : ZNGAvatarImageView
+
+@end

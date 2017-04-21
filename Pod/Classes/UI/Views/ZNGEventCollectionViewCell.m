72751f34c11eb4f5c1f7514b1d976f958a28bb9c
Jason Neel <jason.neel@gmail.com>
2017-04-21 12:07:16 -0700

    Removed now unused corner radius from detailed events collection view cells

--- Pod/Classes/UI/Views/ZNGEventCollectionViewCell.m
+++ Pod/Classes/UI/Views/ZNGEventCollectionViewCell.m
@@ -10,11 +10,4 @@
 
 @implementation ZNGEventCollectionViewCell
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    
-    self.textLabel.clipsToBounds = YES; // clips to bounds must be set for the below corner radius to work
-    self.textLabel.layer.cornerRadius = 6.0;
-}
-
 @end

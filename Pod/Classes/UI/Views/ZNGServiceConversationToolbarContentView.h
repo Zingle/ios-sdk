7f552b59ace1c7afeecb3d3d3c0400d118a12a4f
Jason Neel <jason.neel@gmail.com>
2017-06-21 15:05:30 -0700

    IOS-660: Moved declarations for collapsing button methods and properties into the toolbar content view superclass.  These can be safely called by other toolbars with no effect.

--- Pod/Classes/UI/Views/ZNGServiceConversationToolbarContentView.h
+++ Pod/Classes/UI/Views/ZNGServiceConversationToolbarContentView.h
@@ -10,10 +10,4 @@
 
 @interface ZNGServiceConversationToolbarContentView : ZNGConversationToolbarContentView
 
-@property (nonatomic, strong, nullable) IBOutlet UIButton * revealButton;
-@property (nonatomic, strong, nullable) IBOutlet UIButton * channelSelectButton;
-
-- (void) collapseButtons:(BOOL)animated;
-- (void) expandButtons:(BOOL)animated;
-
 @end

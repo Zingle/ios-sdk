e5c023f0af9b041576a0f94f7819ed6b6aed6b07
Jason Neel <jason.neel@gmail.com>
2018-02-27 10:51:06 -0800

    IOS-798: Added a method to inbox data set builder to determine if a data set can be used in a specific session.  This will allow us to avoid showing something like an assignment inbox for a service that does not have assignment enabled.

--- Pod/Classes/UI/ViewModels/Contacts/ZNGContactDataSetBuilder.m
+++ Pod/Classes/UI/ViewModels/Contacts/ZNGContactDataSetBuilder.m
@@ -8,6 +8,9 @@
 
 #import "ZNGContactDataSetBuilder.h"
 #import "ZNGInboxDataSet.h"
+#import "ZingleAccountSession.h"
+#import "ZNGLabel.h"
+#import "ZNGContactGroup.h"
 
 @implementation ZNGContactDataSetBuilder
 
@@ -57,6 +60,57 @@
              NSStringFromSelector(@selector(searchText)): NSStringFromSelector(@selector(searchText)),
              NSStringFromSelector(@selector(searchMessageBodies)): NSStringFromSelector(@selector(searchMessageBodies)),
              };
+}
+
+- (BOOL) builderCanBeUsedForSession:(ZingleAccountSession *)session
+{
+    if (self.unassigned) {
+        if (![session.service allowsAssignment]) {
+            return NO;
+        }
+    }
+    
+    if ([self.assignedUserId length] > 0) {
+        if (![session.service allowsAssignment]) {
+            return NO;
+        }
+        
+        if ([session userWithId:self.assignedUserId] == nil) {
+            return NO;
+        }
+    }
+    
+    if ([self.assignedTeamId length] > 0) {
+        if (![session.service allowsTeamAssignment]) {
+            return NO;
+        }
+        
+        if ([session.service teamWithId:self.assignedTeamId] == nil) {
+            return NO;
+        }
+    }
+    
+    for (NSString * labelId in self.labelIds) {
+        NSUInteger matchingLabelIndex = [session.service.contactLabels indexOfObjectPassingTest:^BOOL(ZNGLabel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            return ([obj.labelId isEqualToString:labelId]);
+        }];
+        
+        if (matchingLabelIndex == NSNotFound) {
+            return NO;
+        }
+    }
+    
+    for (NSString * groupId in self.groupIds) {
+        NSUInteger matchingGroupIndex = [session.service.contactGroups indexOfObjectPassingTest:^BOOL(ZNGContactGroup * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            return ([obj.groupId isEqualToString:groupId]);
+        }];
+        
+        if (matchingGroupIndex == NSNotFound) {
+            return NO;
+        }
+    }
+    
+    return YES;
 }
 
 @end

f6593c4c044cc7266d174ff6c9940902ffcca061
Jason Neel <jason.neel@gmail.com>
2018-05-10 11:31:07 -0700

    IOS-838: Adjusted user settings defaults to match web

--- Pod/Classes/Models/ZNGUserSettings.h
+++ Pod/Classes/Models/ZNGUserSettings.h
@@ -9,8 +9,18 @@
 
 @interface ZNGUserSettings : MTLModel <MTLJSONSerializing>
 
-// Properties are NSNumber instead of BOOL to allow nullability and YES-defaulting in getter methods
+// Properties are NSNumber instead of BOOL to allow nullability and defaulting in getter methods
+
+/**
+ *  When this is `@YES`, inbox UI should only display users that share one or more teams with the current user.
+ *  Defaults to `@YES`.
+ */
 @property (nonatomic, strong, nullable) NSNumber * showOnlyMyTeammates;
+
+/**
+ *  When this is `@YES`, an unassigned inbox should be available to the user in servies with assignment enabled.
+ *  Defaults to nil.
+ */
 @property (nonatomic, strong, nullable) NSNumber * showUnassignedConversations;
 
 @end

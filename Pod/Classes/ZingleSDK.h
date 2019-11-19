462569d7ea62503f370c22a15e8d9a9fb962f47f
Jason Neel <jason.neel@gmail.com>
2019-11-19 10:24:18 -0800

    Added firebase token setting convenience method to ZingleSDK.  Deprecated APNS token setter.

--- Pod/Classes/ZingleSDK.h
+++ Pod/Classes/ZingleSDK.h
@@ -38,7 +38,13 @@ typedef void (^ZNGErrorHandler)(ZNGError * _Nonnull error);
 /**
  *  Static setter for the push notification device token.  This allows apps to register this push notification token for later use before a ZingleSession has been initialized.
  */
-+ (void) setPushNotificationDeviceToken:(nonnull NSData *)token;
++ (void) setPushNotificationDeviceToken:(nonnull NSData *)token
+__attribute__((deprecated("Notifications via normal APNS has been deprecated in favor of Firebase.  Use `setFirebaseToken:` instead.")));
+
+/**
+ *  Static setter for a Firebase FCM token.  This allows apps to register this push notification token for later use before a ZingleSession has been initialized.
+ */
++ (void) setFirebaseToken:(nonnull NSString *)token;
 
 #pragma mark - Contact access
 
--- Pod/Classes/ZingleSDK.m
+++ Pod/Classes/ZingleSDK.m
@@ -19,6 +19,11 @@ NSString * const ZNGPushNotificationReceived = @"ZNGPushNotificationReceived";
     [ZingleSession setPushNotificationDeviceToken:token];
 }
 
++ (void) setFirebaseToken:(nonnull NSString *)token
+{
+    [ZingleSession setFirebaseToken:token];
+}
+
 + (ZingleContactSession *) contactSessionWithToken:(NSString *)token key:(NSString *)key channelTypeId:(NSString *)channelTypeId channelValue:(NSString *)channelValue
 {
     return [[ZingleContactSession alloc] initWithToken:token key:key channelTypeId:channelTypeId channelValue:channelValue];

0754c76ac921131cc2bf1af32bb9306f7483466f
Jason Neel <jason.neel@gmail.com>
2020-10-15 12:30:59 -0700

    Add option to obscure contact data when the app enters the background

--- Pod/Classes/UI/Controllers/ZNGServiceToContactViewController.h
+++ Pod/Classes/UI/Controllers/ZNGServiceToContactViewController.h
@@ -39,6 +39,14 @@
 @property (nonatomic, assign) BOOL hideContactName;
 
 /**
+ *  Optionally hide all contact and messaging data when the app is entering the background.
+ *  This will prevent contact data from appearing in the app switcher.
+ *
+ *  Defaults to NO.
+ */
+@property (nonatomic, assign) BOOL hideContactDataInBackground;
+
+/**
  *  Setting this flag hides the assignment name, e.g. "Jason" in "Assigned to Jason".
  *  Used for animated transitions.
  */
